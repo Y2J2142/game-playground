60b92dd5af9dbc2a14c0427d077c1e3ee85a69f5
Unknown <Y2J2142@hotmail.com>
2018-06-27 19:20:07 +0200

    Weapon class update

--- game_playground/Weapon.h
+++ game_playground/Weapon.h
@@ -1,18 +1,20 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Character.h"
 class Weapon : sf::Drawable
 {
-protected:
+public:
 	float damage;
 	float range;
 	sf::Texture texture;
 	sf::Sprite sprite;
 	sf::Clock attackSpeedTimer;
 	
-public:
 
-	virtual void attack() = 0;
-	virtual void draw(sf::RenderTarget &, sf::RenderStates) = 0;
+
+	virtual void attack() const = 0;
+	virtual void update(const Character &) = 0;
+	virtual void draw(sf::RenderTarget &, sf::RenderStates) const = 0;
 	Weapon();
 	virtual ~Weapon() = 0;
 
