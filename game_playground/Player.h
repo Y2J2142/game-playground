640a581dcf95591f67bd78b8f998703f72d7f651
Unknown <Y2J2142@hotmail.com>
2018-06-29 13:18:09 +0200

    update moved back to player

--- game_playground/Player.h
+++ game_playground/Player.h
@@ -13,7 +13,7 @@ public:
 	Player(sf::Vector2f, float movespeed, int health, std::string texturePath);
 	~Player();
 
-	void update(sf::RenderWindow& renderWindow, sf::RenderTarget & output, sf::Time time) override;
+	void update(sf::RenderWindow& renderWindow, sf::RenderTarget & output, sf::Time time);
 	void movementHandler();
 };
 
