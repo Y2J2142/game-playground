c4ee4ae9beab6d3c2692b2c46bdecc9cbce0eeb7
Unknown <Y2J2142@hotmail.com>
2018-07-01 18:36:51 +0200

    collision layer added

--- game_playground/TileMap.cpp
+++ game_playground/TileMap.cpp
@@ -14,12 +14,15 @@ TileMap::~TileMap()
 
 bool TileMap::load(const std::string & tileMapPath, std::vector<std::vector<int>> level)
 {
+	this->level.clear();
 	this->level = level;
 	if (!this->tileset.loadFromFile(tileMapPath)) return false;
-	
+	this->collisionLayer.resize(this->level.size());
+	setCollisionLayer();
 	auto width = level.size();
 	auto height = level[0].size();
 
+	
 	vertexArray.setPrimitiveType(sf::Quads);
 	vertexArray.resize(width * height * 4);
 
@@ -56,23 +59,45 @@ void TileMap::draw(sf::RenderTarget & renderTarget, sf::RenderStates states) con
 	renderTarget.draw(vertexArray, states);
 }
 
+void TileMap::setCollisionLayer()
+{
+	collisionLayer.clear();
+	
+	for (auto i = 0; i < level.size(); ++i)
+		collisionLayer.emplace_back(level[0].size());
+	auto size = this->level[0].size();
+	
+	for (auto && c : collisionLayer)
+		fill(std::begin(c), std::end(c), false);
+
+	for(auto i = 0; i < collisionLayer.size(); ++i)
+		for (auto j = 0; j < collisionLayer[0].size(); ++j)
+		{
+			for (const auto tile : TileMap::collisionTiles)
+			{
+				if (level[i][j] == tile)
+					collisionLayer[i][j] = true;
+			}
+		}
+}
+
 void TileMap::restrictMovement(Character & character)
 {
 	auto[x, y] = character.tilePos;
 	std::fill(std::begin(character.canMove), std::end(character.canMove), false);
-	if (level[x - 1][y] == 4)
+	if (collisionLayer[x - 1][y])
 		character.canMove[Character::Direction::LEFT] = false;
 	else
 		character.canMove[Character::Direction::LEFT] = true;
-	if (level[x + 1][y] == 4)
+	if (collisionLayer[x + 1][y])
 		character.canMove[Character::Direction::RIGHT]= false;
 	else
 		character.canMove[Character::Direction::RIGHT] = true;
-	if (level[x][y - 1] == 4)
+	if (collisionLayer[x][y - 1])
 		character.canMove[Character::Direction::UP] = false;
 	else
 		character.canMove[Character::Direction::UP] = true;
-	if (level[x][y + 1] == 4)
+	if (collisionLayer[x][y + 1])
 		character.canMove[Character::Direction::DOWN] = false;
 	else
 		character.canMove[Character::Direction::DOWN] = true;
--- game_playground/TileMap.h
+++ game_playground/TileMap.h
@@ -9,14 +9,15 @@ class TileMap : public sf::Drawable, public sf::Transformable
 	sf::Texture tileset;
 	sf::Vector2u tileSize;
 	std::vector<std::vector<int>> level;
-
+	std::vector<std::vector<bool>> collisionLayer;
+	static constexpr int collisionTiles[] = { 1, 2, 4 };
 public:
 	TileMap();
 	~TileMap();
 
 	bool load(const std::string & tileMapPath, std::vector<std::vector<int>> level);
 	void draw(sf::RenderTarget&, sf::RenderStates)const;
-
+	void setCollisionLayer();
 	void restrictMovement(Character &);
 };
 
